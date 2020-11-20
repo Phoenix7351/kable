e6342d90c34f9550f01df3f542ae31c30cd2b656
Travis Wyatt <travis@juul.com>
2020-11-19 16:47:51 -0800

    Setup binary-compatibility-validator

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ plugins {
     id("com.vanniktech.maven.publish") version "0.13.0" apply false
     id("org.jetbrains.dokka") version "1.4.10.2" apply false
     id("kotlinx-atomicfu") version "0.14.4" apply false
+    id("binary-compatibility-validator") version "0.2.4"
 }
 
 allprojects {
